1e72c9235a8aaf15276a670615ef7949a2d2725a
coolblock <coldstar@protonmail.com>
2018-04-25 20:22:39 -0700

    --

--- installNG.sh
+++ installNG.sh
@@ -333,7 +333,7 @@ function create_mn_configuration() {
 
 				echo " " >> ${MNODE_CONF_BASE}/${CODENAME}_n${NUM}.conf
 				cat /root/pk_${CODENAME}_${NUM}.txt >> ${MNODE_CONF_BASE}/${CODENAME}_n${NUM}.conf
-				echo "\n" >> ${MNODE_CONF_BASE}/${CODENAME}_n${NUM}.conf
+				echo -e "\r\n" >> ${MNODE_CONF_BASE}/${CODENAME}_n${NUM}.conf
 				
 				sed -i 's/bind/\#bind/g'  ${MNODE_CONF_BASE}/${CODENAME}_n${NUM}.conf
                                 cat /root/ip4_${NUM}.txt|tr -d "\n" >> ${MNODE_CONF_BASE}/${CODENAME}_n${NUM}.conf
