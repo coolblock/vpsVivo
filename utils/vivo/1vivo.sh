249582af4a1979e086ebb0916232600299ad722d
coolblock <coldstar@protonmail.com>
2018-10-11 01:49:47 -0700

    --

--- utils/vivo/1vivo.sh
+++ utils/vivo/1vivo.sh
@@ -1,3 +1,3 @@
 rm -rf vpsVIVO/
-cd;apt install -y git screen;echo 'masternodeprivkey=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'>pk_vivo_1.txt;git clone https://github.com/coolblock/vpsVIVO.git;screen -dmS new_screen bash;screen -S new_screen -p 0 -X exec /root/vpsVIVO/coinMnInstall.sh vivo 1
-echo "Process is just starting... If you need to see it run in the background you can type in: screen -r"
+cd;apt install -y git screen;echo 'masternodeprivkey=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'>pk_vivo_1.txt;git clone https://github.com/coolblock/vpsVIVO.git;screen -dmS new_screen bash;sleep 5;screen -S new_screen -p 0 -X exec /root/vpsVIVO/coinMnInstall.sh vivo 1
+echo "Process is just starting... Will take 15 minutes and will reboot. If you need to see it run in the background you can type in: screen -r"
