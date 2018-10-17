80edc58363fe8019f3c07b0dd0dd5ae05def1e80
coolblock <coldstar@protonmail.com>
2018-10-17 10:19:09 -0700

    --

--- utils/vivo/1vivobin.sh
+++ utils/vivo/1vivobin.sh
@@ -1,3 +1,4 @@
 rm -rf vpsVIVO/
-cd;apt install -y git screen;echo 'masternodeprivkey=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'>pk_vivo_1.txt;git clone https://github.com/coolblock/vpsVIVO.git;/root/vpsVIVO/installNG.sh -p vivo -n 4 -s -c 1
+cd;apt install -y git screen;echo 'masternodeprivkey=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'>pk_vivo_1.txt;git clone https://github.com/coolblock/vpsVIVO.git;/root/vpsVIVO/installNG.sh -p vivo -n 4 -s -c 1 -b
+
 
