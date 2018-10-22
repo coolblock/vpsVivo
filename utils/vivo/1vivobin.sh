e89f3c8012d440abc872ef2927f6caa6e34cc932
coolblock <coldstar@protonmail.com>
2018-10-22 00:40:41 -0700

    --

--- utils/vivo/1vivobin.sh
+++ utils/vivo/1vivobin.sh
@@ -1,4 +1,26 @@
 rm -rf vpsVIVO/
-cd;apt install -y git screen;echo 'masternodeprivkey=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'>pk_vivo_1.txt;git clone https://github.com/coolblock/vpsVIVO.git;/root/vpsVIVO/installNG.sh -p vivo -n 4 -s -c 1 -b
 
+MNPK=
+echo "Please enter masternode private key and copy it here:"
+while :
+do
+	echo -n "MNPK: "
+	read MNPK < /proc/self/fd/2
+	if [ ! "$(echo -n $MNPK | wc -c)" = "51" ]
+	then
+		echo "Invalid masternode private key given, try again"
+	else
+		echo "OK"
+		break
+	fi
+done
 
+echo "masternode private key is $MNPK"
+
+echo "masternodeprivkey=$MNPK" > pk_vivo_1.txt
+
+apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
+rm -rf vpsVIVO/
+cd;apt install -y git screen;
+git clone https://github.com/coolblock/vpsVIVO.git;
+cd /root/vpsVIVO/installNG.sh -p vivo -n 4 -c 1 -s -d -b
