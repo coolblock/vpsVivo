0d99ce1fa431807bb2a2043c1315d1e1e1237625
coolblock <coldstar@protonmail.com>
2018-11-27 11:22:10 -0700

    --

---
+++ utils/vivo/1vivobinUpdate.sh
@@ -0,0 +1,15 @@
+rm -rf vpsVIVO/
+
+apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
+rm -rf vpsVIVO/
+cd;apt install -y git screen;
+git clone https://github.com/coolblock/vpsVIVO.git;
+cd vpsVIVO
+rm -rf /usr/local/bin/vivod
+./installNG.sh -p vivo -n 4 -c 1 -s -d -b
+echo "To look at status of the masternode run:"
+echo "/root/vpsVIVO/overAllMnStat.sh"
+echo "in fact, I will run it in 20 seconds ..."
+sleep 20
+/root/vpsVIVO/overAllMnStat.sh
+echo "The masternode will start and stop on its own, it is a service."
