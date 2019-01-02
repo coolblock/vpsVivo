0a0081cbd3dfbb52ae071e6e3a35070da62b9e93
coolblock <coldstar@protonmail.com>
2019-01-02 08:19:33 -0700

    --

--- installNG.sh
+++ installNG.sh
@@ -796,7 +796,7 @@ apt-get install -y git virtualenv
 	rm Vivo-0.12.1.8-ubuntu16.04.tar.gz
 	wget https://github.com/vivocoin/vivo/releases/download/v0.12.1.8/Vivo-0.12.1.8-ubuntu16.04.tar.gz
 	//wget https://github.com/vivocoin/vivo/releases/download/v0.12.1.9/vivo-0.12.1.9-ubuntu16-vps-nogui.tgz
-	tar -xvf vivo-0.12.1.9-ubuntu16-vps-nogui.tgz 
+	tar -xvf Vivo-0.12.1.8-ubuntu16.04.tar.gz
 	
 	if [[ -r /etc/os-release ]]; then
 		. /etc/os-release
@@ -804,7 +804,7 @@ apt-get install -y git virtualenv
 				rm Vivo-0.12.1.8-ubuntu17.10.tar.gz
 				wget https://github.com/vivocoin/vivo/releases/download/v0.12.1.8/Vivo-0.12.1.8-ubuntu17.10.tar.gz
 				//wget https://github.com/vivocoin/vivo/releases/download/v0.12.1.9/vivo-01219-ubuntu18.tar.gz
-				tar -xvf vivo-01219-ubuntu18.tar.gz 
+				tar -xvf Vivo-0.12.1.8-ubuntu17.10.tar.gz
 		fi
 	fi
 	
