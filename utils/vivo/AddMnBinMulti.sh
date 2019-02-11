d260b8455f8f75edee7a3bdb51bc02efe8cc79ec
coolblock <coldstar@protonmail.com>
2019-02-11 14:55:34 -0700

    --

--- utils/vivo/AddMnBinMulti.sh
+++ utils/vivo/AddMnBinMulti.sh
@@ -97,10 +97,9 @@ getMasternodePort() {
 		else
 			echo "$mnport" > mnport_vivo_$index.txt
 			echo "ufw allow $mnport" >> allowport.sh
+		    cp /root/ip4_1.txt /root/ip4_$index.txt			
                 break
 		fi
-
-
 
 	else
 		echo " not an integer"
