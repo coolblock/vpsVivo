8d3768bef4f29cf285c52749e97b30706cba8fec
coolblock <coldstar@protonmail.com>
2019-05-17 15:25:31 -0700

    --

--- utils/vivo/vAutoUpdate.sh
+++ utils/vivo/vAutoUpdate.sh
@@ -1,28 +1,54 @@
-#!/bin/bash
+#!/usr/bin/env bash
+HOME=/root
+LOGNAME=root
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+LANG=en_US.UTF-8
+SHELL=/bin/sh
+PWD=/root
+
 cd
 mkdir /root/vivomisc
 cd /root/vivomisc
 rm -f minver*
 rm -f curver*
-
+	echo "starting" >> /root/cronup.txt
 wget https://raw.githubusercontent.com/vivocoin/centralstrings/master/minver
 /root/mnTroubleshoot/vivo/vivo1_getInfo.sh | grep "\"version"| cut -d: -f2 | cut -d" " -f2 | cut -d"," -f1 > curver
 
 typeset -i currentVersion=$(cat curver)
 typeset -i minimumVersion=$(cat minver)
 
+echo "current:$currentVersion" min: "$minimumVersion" >>  /root/cronup.txt
+
+echo "$( date +%T )" >> /root/cronup.txt
 if [ "$currentVersion" -lt "$minimumVersion" ] ; then
-	echo "needs updating"
-	bash <(curl -s https://raw.githubusercontent.com/coolblock/vpsVivo/master/utils/vivo/UpdateAutomaticMnBinMulti.sh)
+	echo "needs updating" >> /root/cronup.txt
+	rm /usr/local/bin/vivod
+	if [ -f "/usr/local/bin/vivod" ]; then echo "vivod exists"; else echo "vivod does not exists" ; fi
+	/root/vpsVivo/utils/vivo/UpdateAutomaticMnBinMulti.sh
+	echo "done updating $( date +%T )" >> /root/cronup.txt
+	if [ -f "/usr/local/bin/vivod" ]; then echo "vivod exists"; else echo "vivod does not exists" ; fi
+	systemctl daemon-reload
+
 else
-	echo "is updated -no change is necessary"
+	echo "is updated -no change is necessary">> /root/cronup.txt
 fi
 
 cd
 
-	if ! crontab -l | grep "/root/vpsVivo/utils/vivo/vAutoUpdate.sh"; then
-		(crontab -l 2>/dev/null; echo "0 2 * * * /sbin/runuser -l root -c '/root/vpsVivo/utils/vivo/vAutoUpdate.sh'") | crontab -
-	fi
-`
+FLOOR=1;
+CEILING=24;
+RANGE=$(($CEILING-$FLOOR+1));
+RESULT=$RANDOM;
+let "RESULT %= $RANGE";
+RESULT=$(($RESULT+$FLOOR));
+echo "Update check will happen at ($RESULT) hour everyday";
 
+
+	if ! crontab -l | grep "/root/vpsVivo/utils/vivo/vAutoUpdate.sh"; then
+		echo "0 $RESULT * * * /sbin/runuser -l root -c '/root/vpsVivo/utils/vivo/vAutoUpdate.sh'"
+		(crontab -l 2>/dev/null; echo "0 $RESULT * * * /sbin/runuser -l root -c '/root/vpsVivo/utils/vivo/vAutoUpdate.sh'") | crontab -
+	fi
+
+echo "End  $( date +%T )" >> /root/cronup.txt
 
