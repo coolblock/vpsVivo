1e275fdd09ae0023625353f06898abe53b3acf74
coolblock <coldstar@protonmail.com>
2019-06-02 02:36:58 -0700

    --

--- utils/vivo/AddMnBinMulti.sh
+++ utils/vivo/AddMnBinMulti.sh
@@ -98,7 +98,7 @@ getMasternodePort() {
 			echo "$mnport" > mnport_vivo_$index.txt
 			echo "ufw allow $mnport" >> allowport.sh
 			
-			f [ ! -f /root/ip4_1.txt ]; then
+			if [ ! -f /root/ip4_1.txt ]; then
                 ipvariable=$(wget http://ipecho.net/plain -O - -q);
                 echo "bind=$ipvariable" > /root/ip4_1.txt
                 echo "Will be using {$ipvariable} as your IP. If you want to change them you will have to go to etc/masternodes and change the conf file."
